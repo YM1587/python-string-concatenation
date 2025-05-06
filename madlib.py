fa834918c387154d2a047bf0ad599dfbbf152e53
Eugine Onyango <eugineogutu004@gmail.com>
2025-05-06 21:30:40 +0300

    Added madlib file

---
+++ madlib.py
@@ -0,0 +1,10 @@
+noun=input("Noun!")
+adj=input("Adjective!")
+adj1=input("Adjective1!")
+pronoun=input("Pronoun!")
+conjuction=input("Conjuction!")
+madlib=f"It's a new year and people are growing old and getting worried of getting off the block! \
+I personally I'm a {noun}. But it doesn't matter ,we're still {adj}. Anyway ,who doesn't want to get \
+{adj1}. What entices me most is that nothing can stop {pronoun} but the loss of breath and I'm still \
+breathing {conjuction} is still on.!"
+print(madlib)
